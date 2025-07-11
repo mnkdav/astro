3da19ec24d5c301e9f76bc6bdd66aa2a37918bf8
mnkdav <mnkdav9@gmail.com>
2025-07-11 09:09:11 +0900

    Create Home.py

---
+++ Home.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.title("💎 우주에서의 약물 결정화 시뮬레이션")
+
+st.markdown("""
+이 앱은 지구와 우주에서 결정화가 어떻게 다르게 일어나는지를 시각적으로 보여줍니다.\
+중력 환경의 차이가 입자 운동, 결정 성장, 약물 수율에 어떤 영향을 미치는지를 탐구하고,\
+관련 제약 사례를 통해 우주약물개발의 가능성을 조명합니다.
+""")
+
+st.image("https://upload.wikimedia.org/wikipedia/commons/thumb/0/02/Protein_crystal_space_vs_earth.png/800px-Protein_crystal_space_vs_earth.png", caption="우주 vs 지구 결정 구조 비교 예시")
